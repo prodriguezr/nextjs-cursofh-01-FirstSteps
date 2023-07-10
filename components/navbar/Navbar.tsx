070d7e5c3641ffa5707a82b11f754796d85a35f6
Pablo Rodríguez <prodrigu75@gmail.com>
2023-07-09 20:27:21 -0400

    Fix menu items align

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -11,7 +11,7 @@ const navItems: NavItem[] = [
 
 export const Navbar = () => {
   return (
-    <nav className='flex p-2 m-2 bg-blue-800 rounded bg-opacity-30'>
+    <nav className='flex items-center p-2 m-2 bg-blue-800 rounded bg-opacity-30'>
       <Link href='/' className='flex items-center gap-2'>
         <HomeIcon size={36} />
         <span className='text-xl font-bold'>Home</span>
